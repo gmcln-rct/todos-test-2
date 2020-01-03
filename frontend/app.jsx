b9298a5459099eaab7adc807ef7272a8d7aa177b
Glenn McClanan <producersama@gmail.com>
2020-01-03 14:54:07 -0500

    Add to frontend

---
+++ frontend/app.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+export default App = () => (
+    <div className='app'>
+        <h1>A Todos App</h1>
+    </div>
+);
---
+++ frontend/components/root.jsx
@@ -0,0 +1,10 @@
+
+import React from 'react';
+import {Provider} from 'react-redux';
+import App from './app'
+
+export default Root = ({store}) => (
+    <Provider store={store}>
+        <App />
+    </Provider>
+);
