e544081a09613d149a86f128e71044a8abb1413f
Glenn McClanan <producersama@gmail.com>
2020-01-04 17:45:53 -0500

    Set up root

--- frontend/components/root.jsx
+++ frontend/components/root.jsx
@@ -1,7 +1,7 @@
 
 import React from 'react';
 import {Provider} from 'react-redux';
-import App from './app'
+import App from './app';
 
 export default Root = ({store}) => (
     <Provider store={store}>
