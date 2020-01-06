27c2c77ce3a3f82a6de4f07f9413991bad6cc4c4
Glenn McClanan <producersama@gmail.com>
2020-01-06 18:57:24 -0500

    Update text

--- frontend/components/app.jsx
+++ frontend/components/app.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 const App = () => (
     <div className="app">
         <h1>Todo Or Not Todo List</h1>
+        <p>Indecisiveness isn't just for Danish princes anymore.</p>
     </div>
 );
 
