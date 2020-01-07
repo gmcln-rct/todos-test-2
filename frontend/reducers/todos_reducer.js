3c5ce6894c39f83608736ac9c7a517657ac1b825
Glenn McClanan <producersama@gmail.com>
2020-01-07 14:37:39 -0500

    Add remove todo

--- frontend/reducers/todos_reducer.js
+++ frontend/reducers/todos_reducer.js
@@ -1,4 +1,4 @@
-import { RECEIVE_TODOS, RECEIVE_TODO } from '../actions/todo_actions';
+import { RECEIVE_TODOS, RECEIVE_TODO, REMOVE_TODO } from '../actions/todo_actions';
 
 
 const initialState = {
@@ -31,9 +31,13 @@ const todosReducer = ( state= initialState, action) => {
         case RECEIVE_TODO:
             const newTodo = { [action.todo.id]: action.todo };
             return Object.assign({}, state, newTodo);
+        case REMOVE_TODO:
+            const nextState = Object.assign({}, this.state);
+            delete nextState[action.todo.id];
+            return nextState;
         default: 
             return state;
     }
-}
+};
 
 export default todosReducer;
