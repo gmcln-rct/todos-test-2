d2997f9da94b4bc5576a5e5af66c6b7663eaa84e
Glenn McClanan <producersama@gmail.com>
2020-01-06 20:45:27 -0500

    Add list item

--- frontend/components/todos/todo_list.jsx
+++ frontend/components/todos/todo_list.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import TodoListItem from './todo_list_item';
 
 const TodoList = () => <h3>Todo List goes here!</h3>;
 
--- frontend/components/todos/todo_list_item.jsx
+++ frontend/components/todos/todo_list_item.jsx
@@ -1,1 +1,15 @@
 import React from 'react';
+
+class TodoListItem extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        
+    }
+
+
+}
+
+export default TodoListItem;
